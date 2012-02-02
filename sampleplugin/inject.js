0cbea67a6ec81d4a523fd54098d5541a0ee15f26
Alex Lysenko <tywonka@gmail.com>
2012-02-02 22:20:34 +0200

    Adding npapi dll and chrome extension

---
+++ sampleplugin/inject.js
@@ -0,0 +1,5 @@
+﻿var element = document.createElement('embed');
+element.setAttribute('id','pluginObj');
+element.setAttribute('type', 'application/x-helloworld');
+element.setAttribute('hidden', 'true');
+document.body.appendChild(element);
---
+++ sampleplugin/run.js
@@ -0,0 +1,6 @@
+﻿var port = chrome.extension.connect({name: "WinExec"});
+port.onMessage.addListener(function(msg) {
+	var element = document.getElementById('pluginObj');
+	element.sayHello(msg.text);
+});
+port.postMessage({joke: "123"});
